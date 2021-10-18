c03a7388c9f3244c834547ebabfe57610f547dc6
goessm <44338544+goessm@users.noreply.github.com>
2021-10-18 15:18:55 +0200

    fix reference error

--- src/extension-support/argument-type.js
+++ src/extension-support/argument-type.js
@@ -1,4 +1,4 @@
 // This is here to be compatible with the current version of scratch-ui
 // which accesses these fields directly from the vm
-ArgumentType = require('default-scratch-vm/src/extension-support/argument-type');
+ArgumentType = require('scratch-vm-default/src/extension-support/argument-type');
 module.exports = ArgumentType;
--- src/extension-support/block-type.js
+++ src/extension-support/block-type.js
@@ -1,4 +1,4 @@
 // This is here to be compatible with the current version of scratch-ui
 // which accesses these fields directly from the vm
-BlockType = require('default-scratch-vm/src/extension-support/block-type');
+BlockType = require('scratch-vm-default/src/extension-support/block-type');
 module.exports = BlockType;
