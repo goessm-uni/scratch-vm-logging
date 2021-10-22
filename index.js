a365469fd149f0a9f963bda3a727516a13e0ba50
goessm <44338544+goessm@users.noreply.github.com>
2021-10-22 15:02:34 +0200

    log greenFlag and stopAll calls

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const VirtualMachine = require('scratch-vm-default');
 const ScratchLogSender = require('scratch-log-sender')
 
 class LoggingVirtualMachine extends VirtualMachine {
+
+    /**
+     * Overwrite greenFlag to add log call.
+     */
+    greenFlag () {
+        ScratchLogSender.userEventLog.logControlEvent('greenFlag', this.runtime);
+        super.greenFlag();
+    }
+
+    /**
+     * Overwrite stopAll to add log call.
+     */
+    stopAll () {
+        ScratchLogSender.userEventLog.logControlEvent('stopAll', this.runtime);
+        super.stopAll();
+    }
+
     /**
      * Overwrite blockListener to add log call.
      * @param {!Blockly.Event} e Any Blockly event.
